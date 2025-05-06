8520506694783d96e5b451dda8a653d58c8cea43
Vyctoria Karina <vyctoriak@gmail.com>
2025-05-06 14:19:47 -0300

    Add GitHub Actions workflow for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/orgaio-2" : "",
+};
 
-export default nextConfig
+export default nextConfig;
